44205ecf6e3d4251da1506f40c97c48dc7f27a37
ahakits <ahakits1987@gmail.com>
2016-11-13 16:05:09 +0900

    fix popwin.el

--- init/40-popwin.el
+++ init/40-popwin.el
@@ -9,7 +9,7 @@
 
 ;; anything
 (setq anything-samewindow nil)
-(push '("*anything*" :height 60) popwin:special-display-config)
+(push '("*anything*" :height 40) popwin:special-display-config)
 (push '("*anything moccur*" :height 40) popwin:special-display-config)
 (push '("*anything kill-ring*" :height 40) popwin:special-display-config)
 
